aeb380e20a114296424c2ed6dbe4c8f68175043f
giorgi <gjanashia@unisens.ge>
2023-01-17 15:37:00 +0400

    feat: read contract balances from wallet address

---
+++ services/readContract.js
@@ -0,0 +1,33 @@
+const { ethers } = require('ethers');
+require('dotenv').config({ path: '../.env' });
+
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+);
+
+const ERC20_ABI = [
+  'function name() view returns (string)',
+  'function symbol() view returns (string)',
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address) view returns (int)',
+];
+
+const contract = new ethers.Contract(
+  process.env.CONTRACT_ADDRESS,
+  ERC20_ABI,
+  provider
+);
+
+(async () => {
+  const name = await contract.name();
+  const contractSymbol = await contract.symbol();
+  const totalSupply = await contract.totalSupply();
+  const balance = await contract.balanceOf(process.env.WALLET_ADDRESS);
+
+  console.log({
+    name,
+    symbol: contractSymbol,
+    totalSupply: ethers.utils.formatEther(totalSupply),
+    balance: ethers.utils.formatEther(balance),
+  });
+})();
